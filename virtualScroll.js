086db3ed0ef89149e4f435d0551c4dbd38825cd4
张玉鑫 <yuxin.zhang@kalendsoft.com>
2022-06-29 11:32:20 +0800

    又写了一遍

--- virtualScroll.js
+++ virtualScroll.js
@@ -1,221 +1,220 @@
 class VirtualScroll {
-  constructor(c, list, itemGenerator, cacheCount) {
-    // console.log(c)
-    this.container = c
-    this.list = list
-    this.cacheList = []
-    this._offset = 0
-    this.offsetToEdge = 0
-    this.cacheCount = cacheCount || 6
-    this.itemGenerator = itemGenerator
-    this.contentHeight = 0
+    constructor(c, list, itemGenerator, cacheCount) {
+        this.container = c; // container
+        this.list = list; // raw list
+        this.cacheList = []; // cache list
+        this._offset = 0; // offset
+        this.offsetToEdge = 0; // offsettoedge
+        this.cacheCount = cacheCount || 5; //前后缓存个数
+        this.itemGenerator = itemGenerator; // 生产列表元素
+        this.contentHeight = 0; // 列表元素总宽度
 
-    this.initItem(list)
-    this.initContainer(c)
-    this.bindEvents()
+        this.initItem(list);
+        this.initContainer(c);
+        this.bindEvents();
 
-    this.offset = this._offset
-  }
-
-  get offset() {
-    return this._offset
-  }
-
-  set offset(val) {
-    this.render(val)
-    this._offset = val
-    return true
-  }
-  // 转换数组，附加上高度和index
-  initItem(list) {
-    this._list = list.map((item, index) => {
-      return {
-        item: item,
-        height: 30,
-        index: index
-      }
-    })
-  }
-
-  // 初始化containerHeight和container overflow:hidden
-  initContainer(container) {
-    this.containerHeight = container.clientHeight
-    this.contentHeight = calc(this._list)
-    this.container.style.overflow = 'hidden'
-  }
-
-  bindEvents() {
-    let y = 0
-    // 滚动的最大高度
-    const maxOffset = this.contentHeight - this.containerHeight
-    const _updateY = (event) => {
-      event.preventDefault()
-      y += event.deltaY
-      // 向上为负值，但y最小为0
-      y = Math.max(y, 0)
-      // 最大偏移量为maxOffset
-      y = Math.min(y, maxOffset)
+        this.offset = this._offset;
     }
 
-    const updateOffset = () => {
-      if (y !== this.offset) this.offset = y
+    get offset() {
+        return this._offset;
     }
 
-    const _updateOffset = throttle(updateOffset, 30)
-
-    // 更新y
-    this.container.addEventListener('mousewheel', _updateY)
-    //   更新offset
-    this.container.addEventListener('mousewheel', _updateOffset)
-
-    // 解除绑定事件
-    this.unbindEvents = () => {
-      this.container.removeEventListener('mousewheel', _updateY)
-      this.container.removeEventListener('mousewheel', _updateOffset)
-    }
-  }
-
-  render(_offset) {
-    const headIndex = findIndex(this._list, _offset)
-    const tailIndex = findIndex(this._list, _offset + this.containerHeight)
-
-    if (withCache(headIndex, tailIndex, this.cacheList)) {
-      const headIndexWithCache = this.cacheList[0].index
-      const offsetToEdge = _offset - calc(this._list, 0, headIndexWithCache)
-      this.itemWrapper.style.transform = `translateY(-${offsetToEdge}px)`
-      return
-    }
-    console.log('重新渲染')
-    // const heightSum = calc(this._list, 0, headIndex)
-
-    //   根据当前head和tail计算缓存列表的首尾index
-    const headIndexWithCache = Math.max(headIndex - this.cacheCount, 0)
-    const tailIndexWithCache = Math.min(
-      tailIndex + this.cacheCount,
-      this._list.length - 1
-    )
-
-    //   计算缓存列表
-    this.cacheList = this._list.slice(headIndexWithCache, tailIndexWithCache)
-
-    this.offsetToEdge = _offset - calc(this._list, 0, headIndexWithCache)
-
-    // this.renderList = this._list.slice(headIndex, tailIndex + 1)
-    if (!this.itemWrapper) {
-      const itemWrapper = document.createElement('div')
-      itemWrapper.classList.add('vs__inner')
-      this.container.appendChild(itemWrapper)
-      this.itemWrapper = itemWrapper
+    set offset(val) {
+        this.render(val);
+        this._offset = val;
+        return true;
     }
 
-    const fragment = document.createDocumentFragment()
+    // 转换数组，附加上高度和index
+    initItem(list) {
+        this._list = list.map((item, index) => {
+            return {
+                item: item,
+                height: 30,
+                index: index,
+            };
+        });
+    }
 
-    //   根据缓存列表添加元素至fragement
-    this.cacheList.forEach((v) => {
-      const item = this.itemGenerator(v)
-      fragment.appendChild(item)
-    })
+    // 初始化containerHeight和container overflow:hidden
+    initContainer(container) {
+        this.containerHeight = container.clientHeight;
+        this.contentHeight = calc(this._list);
+        this.container.style.overflow = "hidden";
+    }
 
-    // this.renderList.forEach((v) => {
-    //   const item = this.itemGenerator(v)
-    //   fragment.appendChild(item)
-    // })
+    bindEvents() {
+        let y = 0;
+        // 滚动的最大高度
+        const maxOffset = this.contentHeight - this.containerHeight;
+        const _updateY = (event) => {
+            event.preventDefault();
+            y += event.deltaY;
+            // 向上为负值，但y最小为0
+            y = Math.max(y, 0);
+            // 最大偏移量为maxOffset
+            y = Math.min(y, maxOffset);
+        };
 
-    this.itemWrapper.innerHTML = ''
+        const updateOffset = () => {
+            if (y !== this.offset) this.offset = y;
+        };
 
-    this.itemWrapper.appendChild(fragment)
+        const _updateOffset = throttle(updateOffset, 30);
 
-    //   每次监听到事件都重新渲染，然后计算出向上偏移量
-    this.itemWrapper.style.transform = `translateY(-${this.offsetToEdge}px)`
+        // 更新y
+        this.container.addEventListener("mousewheel", _updateY);
+        //   更新offset
+        this.container.addEventListener("mousewheel", _updateOffset);
 
-    // 生成虚拟列表并保存到wrapper
-  }
+        // 解除绑定事件
+        this.unbindEvents = () => {
+            this.container.removeEventListener("mousewheel", _updateY);
+            this.container.removeEventListener("mousewheel", _updateOffset);
+        };
+    }
+
+    render(_offset) {
+        const headIndex = findIndex(this._list, _offset);
+        const tailIndex = findIndex(this._list, _offset + this.containerHeight);
+
+        if (withCache(headIndex, tailIndex, this.cacheList)) {
+            const headIndexWithCache = this.cacheList[0].index;
+            const offsetToEdge = _offset - calc(this._list, 0, headIndexWithCache);
+            this.itemWrapper.style.transform = `translateY(-${offsetToEdge}px)`;
+            return;
+        }
+
+        console.log("重新渲染");
+
+        //   根据当前head和tail计算缓存列表的首尾index
+        const headIndexWithCache = Math.max(headIndex - this.cacheCount, 0);
+        const tailIndexWithCache = Math.min(
+            tailIndex + this.cacheCount,
+            this._list.length - 1
+        );
+
+        //   计算缓存列表
+        this.cacheList = this._list.slice(headIndexWithCache, tailIndexWithCache);
+
+        // wrapper应该偏移的距离
+        this.offsetToEdge = _offset - calc(this._list, 0, headIndexWithCache);
+
+        // this.renderList = this._list.slice(headIndex, tailIndex + 1)
+        if (!this.itemWrapper) {
+            const itemWrapper = document.createElement("div");
+            itemWrapper.classList.add("vs__inner");
+            this.container.appendChild(itemWrapper);
+            this.itemWrapper = itemWrapper;
+        }
+
+        const fragment = document.createDocumentFragment();
+
+        //   根据缓存列表添加元素至fragement
+        this.cacheList.forEach((v) => {
+            const item = this.itemGenerator(v);
+            fragment.appendChild(item);
+        });
+
+        // this.renderList.forEach((v) => {
+        //   const item = this.itemGenerator(v)
+        //   fragment.appendChild(item)
+        // })
+
+        this.itemWrapper.innerHTML = "";
+
+        this.itemWrapper.appendChild(fragment);
+        //   若监听到事件后重新渲染，然后计算出wrapper的向上偏移量
+        this.itemWrapper.style.transform = `translateY(-${this.offsetToEdge}px)`;
+
+        // 生成虚拟列表并保存到wrapper
+    }
 }
 
-// function throttle(f, delay) {
-//   let timer, lastTime
+function throttle(f, delay) {
+    let timer, lastTime;
+    return function(...args) {
+        const now = Date.now();
+        // 第一次立刻执行
+        if (!lastTime) {
+            lastTime = now;
+            f.apply(f, args);
+            return;
+        }
+        if (timer) {
+            return;
+        }
 
-//   return function (...args) {
-//     const now = Date.now()
-//     // 第一次立刻执行
-//     if (!lastTime) {
-//       lastTime = now
-//       f.apply(f, args)
-//       return
+        // 如果间隔时间比delay久，那么立即执行f
+        const remain = now - lastTime > delay ? 0 : delay;
+
+        timer = setTimeout(() => {
+            f.apply(this, args);
+            // 每次执行完更新lastTime，以保证间隔
+            lastTime = Date.now();
+            timer = null;
+        }, remain);
+    };
+}
+
+// function throttle(fn, wait) {
+//     let timer, lastApply
+
+//     return function(...args) {
+//         const now = Date.now()
+//         if (!lastApply) {
+//             lastApply = now
+//             fn.apply(this, args)
+
+//             return
+//         }
+
+//         if (timer) return
+//         const remain = now - lastApply > wait ? 0 : wait
+
+//         timer = setTimeout(() => {
+//             fn.apply(this, args)
+//             lastApply = Date.now()
+//             timer = null
+//         }, remain)
 //     }
-
-//     if (timer) {
-//       return
-//     }
-
-//     // 如果间隔时间比delay久，那么立即执行f
-//     const remain = now - lastTime > delay ? 0 : delay
-
-//     timer = setTimeout(() => {
-//       f.apply(this, args)
-//       // 每次执行完更新lastTime，以保证间隔
-//       lastTime = Date.now()
-//       timer = null
-//     }, remain)
-//   }
 // }
-
-function throttle(fn, wait) {
-  let timer, lastApply
-
-  return function (...args) {
-    const now = Date.now()
-    if (!lastApply) {
-      lastApply = now
-      fn.apply(this, args)
-
-      return
-    }
-
-    if (timer) return
-    const remain = now - lastApply > wait ? 0 : wait
-
-    timer = setTimeout(() => {
-      fn.apply(this, args)
-      lastApply = Date.now()
-      timer = null
-    }, remain)
-  }
-}
 
 // 找比高度和比offset大的那个元素
 function findIndex(list, offset) {
-  let height = 0
-  for (let i = 0; i < list.length; i++) {
-    height += list[i].height
+    let height = 0;
+    for (let i = 0; i < list.length; i++) {
+        height += list[i].height;
 
-    if (height > offset) return i
-  }
+        if (height > offset) return i;
+    }
 
-  // 不存在元素高度和大于offset时，说明二者相等
-  return list.length - 1
+    // 不存在元素高度和大于offset时，说明二者相等
+    return list.length - 1;
 }
 
 //  计算list从start到end的高度
 function calc(list, start = 0, end = list.length) {
-  let height = 0
-  for (let i = start; i < end; i++) {
-    height += list[i].height
-  }
+    let height = 0;
+    for (let i = start; i < end; i++) {
+        height += list[i].height;
+    }
 
-  return height
+    return height;
 }
 
+// 是否在cache中
 function withCache(head, end, cacheList) {
-  if (!cacheList.length) return false
+    if (!cacheList.length) return false;
 
-  const cacheHead = cacheList[0]
-  const cacheEnd = cacheList[cacheList.length - 1]
+    const cacheHead = cacheList[0];
+    const cacheEnd = cacheList[cacheList.length - 1];
 
-  const within = (num, min, max) => num >= min && num <= max
+    const within = (num, min, max) => num >= min && num <= max;
 
-  return (
-    within(head, cacheHead.index, cacheEnd.index) &&
-    within(end, cacheHead.index, cacheEnd.index)
-  )
+    return (
+        within(head, cacheHead.index, cacheEnd.index) &&
+        within(end, cacheHead.index, cacheEnd.index)
+    );
 }
